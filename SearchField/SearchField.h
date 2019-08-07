8c9a863043b6d9e7d166f19d3caf75eef9eb6bec
mustii <mustii@mmk.one>
2019-08-07 21:02:08 +0300

    Initial Commit

---
+++ SearchField/SearchField.h
@@ -0,0 +1,19 @@
+//
+//  SearchField.h
+//  SearchField
+//
+//  Created by Mustafa Khalil on 8/7/19.
+//  Copyright © 2019 Molteo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SearchField.
+FOUNDATION_EXPORT double SearchFieldVersionNumber;
+
+//! Project version string for SearchField.
+FOUNDATION_EXPORT const unsigned char SearchFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchField/PublicHeader.h>
+
+
